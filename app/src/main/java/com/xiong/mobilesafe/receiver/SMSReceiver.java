341470ff6bfd74f6771bf72d7514a418768d5cfa
xiongchaoxiangGitHub <1095092510@qq.com>
2017-08-03 06:26:41 +0800

    at home commit to origin,the second

--- app/src/main/java/com/xiong/mobilesafe/receiver/SMSReceiver.java
+++ app/src/main/java/com/xiong/mobilesafe/receiver/SMSReceiver.java
@@ -3,8 +3,12 @@ package com.xiong.mobilesafe.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.telephony.SmsMessage;
 import android.util.Log;
+
+import com.xiong.R;
 
 /**
  * Created by Administrator on 2017/8/1.
@@ -12,22 +16,43 @@ import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSReceiver";
+    private SharedPreferences sp;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         //接收短信代码
         Object[] objs = (Object[]) intent.getExtras().get("pdus");
 
+        sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
         for (Object b : objs) {
             //具体莫一条短信
             SmsMessage sms = SmsMessage.createFromPdu((byte[]) b);
             // 发送者
             String sender = sms.getOriginatingAddress();
-            String body = sms.getMessageBody();
-            if("#*location*#".equals(body)) {
-                //得到GPS
-                Log.i(TAG,"收到防盗短信");
+            String safenumber = sp.getString("safenumber", "");
+            if (sender.contains(safenumber)) {
+//                Toast.makeText(context, sender, Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "====sneder=====" + sender);
+                String body = sms.getMessageBody();
+                if ("#*location*#".equals(body)) {
+                    Log.i(TAG, "得到手机的GPS");
+                    abortBroadcast();
+                } else if ("#*alarm*#".equals(body)) {
+                    Log.i(TAG, "播放报警音乐");
+                    MediaPlayer player = MediaPlayer.create(context, R.raw.ylzs);
+                    player.setLooping(false);
+                    player.setVolume(1.0f, 1.0f);
+                    player.start();
+                    abortBroadcast();
+                } else if ("#*wipedata*#".equals(body)) {
+                    Log.i(TAG, "远程清除数据");
+                    abortBroadcast();
+                } else if ("#*lockscreen*#".equals(body)) {
+                    Log.i(TAG, "远程锁屏");
+                    abortBroadcast();
+                }
             }
+
         }
     }
 }
