c31a8f960e8111e9fd3db53dd19892acf61014d5
xiongchaoxiangGitHub <1095092510@qq.com>
2017-08-05 06:16:51 +0800

    at home commit to origin,the second

---
+++ app/src/main/java/com/xiong/mobilesafe/ui/LockScreen.java
@@ -0,0 +1,28 @@
+package com.xiong.mobilesafe.ui;
+
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.xiong.R;
+
+/**
+ * Created by 99270 on 2017/8/5.
+ */
+
+public class LockScreen extends Activity {
+    private DevicePolicyManager dpm;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lock_screen);
+
+        dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+    }
+
+    public void lockscreen(View view) {
+        dpm.lockNow();
+    }
+}
